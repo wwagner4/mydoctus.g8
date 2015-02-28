57157cac04175834f3d816c4234276e78b66bfb9
wwagner4 <wwagner4@gmail.com>
2015-02-28 18:38:29 +0100

    utest is core testframework

--- src/main/g8/project/$name__Camel$Build.scala
+++ src/main/g8/project/$name__Camel$Build.scala
@@ -33,15 +33,17 @@ object $name;format="Camel"$Build extends Build {
       commonSettings ++
         Seq(
           libraryDependencies += "net.entelijan" %%% "doctus-core" % D.doctusVersion,
-          libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % "test",
-          testFrameworks += new TestFramework("utest.runner.Framework"))
+          libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % "test"
+//          testFrameworks += new TestFramework("utest.runner.Framework")
+          )
 
     lazy val swingSettings =
       commonSettings ++
         Seq(
           fork := true,
           libraryDependencies += "net.entelijan" %% "doctus-swing" % D.doctusVersion,
-          libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.1" % "test")
+          libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % "test",
+          testFrameworks += new TestFramework("utest.runner.Framework"))
 
     lazy val scalajsSettings =
       commonSettings ++
@@ -51,8 +53,9 @@ object $name;format="Camel"$Build extends Build {
           libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0",
           libraryDependencies += "org.scala-js" %% "scalajs-library" % "0.6.0", 
           libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % "test",
-          libraryDependencies += "net.entelijan" %%% "doctus-scalajs" % D.doctusVersion,
-          testFrameworks += new TestFramework("utest.runner.Framework"))
+          libraryDependencies += "net.entelijan" %%% "doctus-scalajs" % D.doctusVersion
+//          testFrameworks += new TestFramework("utest.runner.Framework")
+          )
 
   }
 
