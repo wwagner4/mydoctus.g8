ecd90d743787e6414837b29102b044b4b46aa50c
wwagner4 <wwagner4@gmail.com>
2016-03-12 14:39:09 +0100

    switched to template

--- src/main/g8/$name__camel$-scalajs/src/main/scala/$organization__packaged$/$name__Camel$ScalaJs.scala
+++ src/main/g8/$name__camel$-scalajs/src/main/scala/$organization__packaged$/$name__Camel$ScalaJs.scala
@@ -4,19 +4,24 @@ import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom
 import org.scalajs.dom.raw.HTMLCanvasElement
 import doctus.scalajs._
+import doctus.core.template._
 
 @JSExport("$name;format="Camel"$ScalaJs")
 object $name;format="Camel"$ScalaJs {
 
   @JSExport
   def main() {
+    
     val canvasElem: HTMLCanvasElement = dom.document.getElementById("canvas").asInstanceOf[HTMLCanvasElement]
 
-    val canvas = DoctusCanvasScalajs(canvasElem)
+    val canvas = DoctusTemplateCanvasScalajs(canvasElem)
     val sched = DoctusSchedulerScalajs
-    val pointable = DoctusPointableScalajs(canvasElem)
 
-    $organization$.Controller(canvas, sched, pointable)
+    // Common to all platforms
+    val templ = $organization$.$name;format="Camel"$DoctusTemplate(canvas)
+    DoctusTemplateControllerImpl(templ, sched, canvas)
+
+    
   }
 
 }
--- src/main/g8/$name__camel$-swing/src/main/scala/$organization__packaged$/$name__Camel$Swing.scala
+++ src/main/g8/$name__camel$-swing/src/main/scala/$organization__packaged$/$name__Camel$Swing.scala
@@ -4,17 +4,16 @@ import java.awt.BorderLayout
 import java.awt.Dimension
 import javax.swing.JFrame
 import javax.swing.JPanel
-
 import doctus.swing._
+import doctus.core.template._
 
 object $name;format="Camel"$Swing extends App {
 
   val top = new JFrame()
   val panel = DoctusComponentFactory.component
 
-  val canvas = DoctusCanvasSwing(panel)
+  val canvas = DoctusTemplateCanvasSwing(panel)
   val sched = DoctusSchedulerSwing
-  val pointable = DoctusPointableSwing(panel)
 
   val cp = new JPanel
   cp.setLayout(new BorderLayout)
@@ -26,7 +25,9 @@ object $name;format="Camel"$Swing extends App {
   top.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
   top.setVisible(true)
 
-  Controller(canvas, sched, pointable)
+  // Common to all platforms
+  val templ = $name;format="Camel"$DoctusTemplate(canvas)
+  DoctusTemplateControllerImpl(templ, sched, canvas)
 
 }
 
