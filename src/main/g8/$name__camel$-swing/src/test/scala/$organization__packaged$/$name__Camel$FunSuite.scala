bb5378837f1a87fac8a4d87af6556dfa81e8bb69
wwagner4 <wwagner4@gmail.com>
2016-04-02 21:38:43 +0200

    fixed definition of g8 variable

--- src/main/g8/$name__camel$-swing/src/test/scala/$organization__packaged$/$name__Camel$FunSuite.scala
+++ src/main/g8/$name__camel$-swing/src/test/scala/$organization__packaged$/$name__Camel$FunSuite.scala
@@ -2,7 +2,7 @@ package net.entelijan
 
 import org.scalatest.FunSuite
 
-class $name__Camel$FunSuite extends FunSuite {
+class $name;format="Camel"$FunSuite extends FunSuite {
   
   test("your test") {
     // here comes your test creativity
